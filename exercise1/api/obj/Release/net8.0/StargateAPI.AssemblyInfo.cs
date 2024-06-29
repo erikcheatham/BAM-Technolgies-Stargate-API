47ba1373c05789cdd43aae500d8f0d61a6b1a0b2
Erik Cheatham <erikcheatham@gmail.com>
2024-06-28 23:11:54 -0500

    Code Push Initial Commit

--- exercise1/api/obj/Release/net8.0/StargateAPI.AssemblyInfo.cs
+++ exercise1/api/obj/Release/net8.0/StargateAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StargateAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9b1eb5e108aeb2384429ffdd98d89858eed0e53")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b72d63ed494597e501c5287812b3711b1803eb0")]
 [assembly: System.Reflection.AssemblyProductAttribute("StargateAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StargateAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
